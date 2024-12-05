b9c35b7f6ea67810aa8da4f0b8e4dacd2dad48f5
gcamow <gcamow@gmail.com>
2024-12-04 20:55:00 -0500

    Update updates.js

--- updates.js
+++ updates.js
@@ -1,7 +1,7 @@
 updates = {
     //example
     //message: `Dear loyal customers, Grove City Area Meals on Wheels will be closed on Monday, 11/4/24 due to inclement weather. Thank you for your patience and flexibility.`,
-    message: `Dear customers, the heat is not working at Holy Trinity, meals will be delayed.`,
+    message: `Dear customers, we are closed today due to the bad weather.'
 
     //example - december 5, 2024 at 11:59 PM. Make sure standard 12-hour time is converted to 24-hour time
     //expires: {
