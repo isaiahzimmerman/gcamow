4da3205fde259bd31be3f02b1a6c24e2dcd72aca
gcamow <gcamow@gmail.com>
2024-12-29 21:02:40 -0500

    Update updates.js

--- updates.js
+++ updates.js
@@ -1,7 +1,7 @@
 updates = {
     //example
     //message: `Dear loyal customers, Grove City Area Meals on Wheels will be closed on Monday, 12/29/24 due to a broken sump pump. Thank you for your patience and flexibility.`,
-    message: `Dear loyal customers, we will be closed on Monday, 12/30/24, due to a broken sump pump.` Thank you for your patience and flexibility.`,  
+    message: `Dear loyal customers, we will be closed on Monday, 12/30/24, due to not being able to use water. Not sure when repairs can be done.  Thank you for your patience and flexibility. We will keep you updated.`,  
     //expires: 
     //     month: 12,
     //     day: 5,
