70a9fe8c26a5a58d11da692e3862722c83be6709
gcamow <gcamow@gmail.com>
2024-12-29 20:55:26 -0500

    Update updates.js

--- updates.js
+++ updates.js
@@ -1,14 +1,13 @@
 updates = {
     //example
     //message: `Dear loyal customers, Grove City Area Meals on Wheels will be closed on Monday, 12/29/24 due to a broken sump pump. Thank you for your patience and flexibility.`,
-    message: `Dear loyal customers, we will be closed on Monday, 12/30/24, due to a broken sump pump.`,
-
-    //example - december 5, 2024 at 11:59 PM. Make sure standard 12-hour time is converted to 24-hour time
-    //expires: {
+    message: `Dear loyal customers, we will be closed on Monday, 12/30/24, due to a broken sump pump.`, Thank you for your patience and flexibility.`,  
+    //expires: 
     //     month: 12,
     //     day: 5,
     //     year: 2024,
-    //     hour: 23,
+    //     h
+our: 23,
     //     minute: 59,
     // },
     expires: {
