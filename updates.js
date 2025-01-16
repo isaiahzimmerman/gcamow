989ec62e25b4973e4207a5963f391260d09df420
gcamow <gcamow@gmail.com>
2025-01-15 23:05:26 -0500

    Update updates.js

--- updates.js
+++ updates.js
@@ -1,7 +1,7 @@
 updates = {
     //example
     //message: `Dear loyal customers, Grove City Area Meals on Wheels will be closed on Monday, 12/16/24 due to a bad weather. Thank you for your patience and flexibility.`,
-    message: `Dear loyal customers, we are in need of a driver that can deliver on Tuesdays for a Grove City route.  Please call 724-458-5371, if you are interested.  Thank you.`,  
+    message: `We are currently looking for a volunteer to drive/ deliver meals on Tuesdays, in the Grove City area.  We can also use fill-in drivers for Mercer and Slippery Rock.  Please call 724-458-5371, if you are interested.  Thank you.`,  
     //expires: 
     //     month: 12,
     //     day: 5,
