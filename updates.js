62aa78eb89562152697dda168e0171fd24900b2a
gcamow <gcamow@gmail.com>
2025-06-17 10:00:40 -0400

    Update updates.js

--- updates.js
+++ updates.js
@@ -1,19 +1,18 @@
 updates = {
     //example
-    //message: `Dear loyal customers, Grove City Area Meals on Wheels will be closed on Monday, 12/16/24 due to a bad weather. Thank you for your patience and flexibility.`,
-    //message:  `We are currently looking for a volunteer to drive/deliver meals on Tuesdays, in the Grove City area.  We can also use fill-in drivers for Mercer and Slippery Rock.  Please call 724-458-5371, if you are interested.`,  
+    //message: Please consider giving to the Grove City Area Meals on Wheels, between June 21th - June 27th.  Donations will go to the Community Foundation of Western PA and Eastern OH and matched.  
     //expires: 
-    //     month: 12,
-    //     day: 5,
-    //     year: 2024,
-    //     hour: 23,
-    //     minute: 59,
+    //     month: 6
+    //     day: 27
+    //     year:2025
+    //     hour: 12
+    //     minute:00
     // },
     expires: 
-        month: 2,
-        day: 1,
-        year: 2025,
-        hour: 12,
-        minute: 00,
+        month: 6
+        day: 27
+        year: 2025
+        hour:12
+        minute: 00
     },
 }
