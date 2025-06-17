921d28acbe9036baa8115154b336488bc37cd282
gcamow <gcamow@gmail.com>
2025-06-17 12:20:10 -0400

    Update updates.js

--- updates.js
+++ updates.js
@@ -1,14 +1,15 @@
 updates = {
     //example
     //message: `Please consider giving to the Grove City Area Meals on Wheels, between June 21th - June 27th.  Donations will go to the Community Foundation of Western PA and Eastern OH and matched.` 
-    //expires: 
+    //example:
+    //expires: (
     //     month: 6,
     //     day: 21,
     //     year:2025,
     //     hour: 12,
     //     minute:0,
     // },
-    expires: 
+    expires: (
         month: 6,
         day: 27,
         year: 2025,
