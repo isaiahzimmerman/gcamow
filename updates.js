e7524d278210a8616c8a669ecd7146a22bc4b597
gcamow <gcamow@gmail.com>
2025-06-17 11:14:21 -0400

    Update updates.js

--- updates.js
+++ updates.js
@@ -3,7 +3,7 @@ updates = {
     //message: Please consider giving to the Grove City Area Meals on Wheels, between June 21th - June 27th.  Donations will go to the Community Foundation of Western PA and Eastern OH and matched.  
     //expires: 
     //     month: 6
-    //     day: 27
+    //     day: 21
     //     year:2025
     //     hour: 12
     //     minute:00
